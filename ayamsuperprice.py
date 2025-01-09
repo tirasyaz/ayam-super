3e838b69b9fe0cc0a6c3740d6b9bb4a31565825f
athirah syazwani <93869166+tirasyaz@users.noreply.github.com>
2025-01-09 23:51:37 +0800

    Update ayamsuperprice.py

--- ayamsuperprice.py
+++ ayamsuperprice.py
@@ -10,7 +10,7 @@ st.title("Negeri Sembilan Population and Price Analysis")
 
 # GitHub raw file URLs
 population_file_url = 'https://raw.githubusercontent.com/tirasyaz/ayam-super/refs/heads/main/filtered_population_district.csv'
-price_file_url = 'https://raw.githubusercontent.com/tirasyaz/repository-name/main/Filtered/filtered_pricecatcher_data.csv'
+price_file_url = 'https://raw.githubusercontent.com/tirasyaz/ayam-super/refs/heads/main/filtered_pricecatcher_data.csv'
 
 # Function to load CSV from URL
 def load_data_from_github(url):
